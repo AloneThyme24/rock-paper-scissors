753566c2c8fc2ced8db62bd20fa9b898c3995c04
Emily <104387597+AloneThyme24@users.noreply.github.com>
2025-07-03 16:53:02 -0700

    Add getComputerChoice() to script.js
    - This is the starting point for the project.
    - Adding comments to map out what needs to be done by this function.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("Hello World")
+/* check to see if JavaScript is linked correctly
 
+console.log("Hello World") */
+
+// create a function called getComputerChoice
+// this function will randomly return string values "rock", "paper", or "scissors"
+function getComputerChoice() {
+
+}
