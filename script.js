7f92f92a619dff2cda45dc65523e5f62113f8077
Emily <104387597+AloneThyme24@users.noreply.github.com>
2025-07-09 15:24:15 -0700

    Create a function that returns values
    - Values created were "rock", "paper", "scissors" (RPS)
    - Math.random() was used so it generates a number between 0 and 1
    - Each RPS value was assigned a 1/3 chance of happening using if/else statements

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ console.log("Hello World") */
 // create a function called getComputerChoice
 // this function will randomly return string values "rock", "paper", or "scissors"
 function getComputerChoice() {
+const readySetGo = Math.random(); // generates a number between 0 and 1; each if/else statement gives each option a 1/3 chance of happening
 
+if (readySetGo < 0.33) {
+    return "rock"
+} else if (readySetGo < 0.66) {
+    return "paper"
+} else {
+    return "scissors"
 }
+}
+
+/* test function 
+console.log (getComputerChoice()) */
